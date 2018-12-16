093caf2657801e96543e68edc3f36f514ec54a54
Nav <navyasuri@nyu.edu>
2018-12-16 23:45:12 +0400

    Smartscrolling added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 //var landing = document.getElementsByClassName("landing")[0];
 //landing.style.height = screen.height;
+
+$(document).ready(function () {
+    var options = {
+        mode: "vp", // "vp", "set"
+        autoHash: false,
+        sectionScroll: true,
+        initialScroll: false,
+        keepHistory: false,
+        sectionWrapperSelector: ".section-wrapper",
+        sectionClass: "section",
+        animationSpeed: 500,
+        headerHash: "header",
+        breakpoint: null,
+        eventEmitter: null,
+        dynamicHeight: false
+    };
+
+    $.smartscroll(options);
+    
+});
